3b636b303f3d6b6feefa0040b379f376359aae45
Terrance Griffith <tjgriffi357@gmail.com>
2019-07-02 14:53:28 -0700

    Interview ID
    Created a test ID view for the interview question

---
+++ ToccaTest/ToccaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToccaTest
+//
+//  Created by Terrance Griffith on 7/2/19.
+//  Copyright © 2019 Terrance Griffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
