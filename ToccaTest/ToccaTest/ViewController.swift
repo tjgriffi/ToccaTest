01086683e3d4f3946ba7569fa8494ba4e5eea4c7
Terrance Griffith <tjgriffi357@gmail.com>
2019-07-03 11:44:47 -0700

    Minor changes
    Made some changes to the overall look of the application

--- ToccaTest/ToccaTest/ViewController.swift
+++ ToccaTest/ToccaTest/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mToolBar: UIToolbar!
+    @IBOutlet var contactsButton: UIBarButtonItem!
+    @IBOutlet var myCardButton: UIBarButtonItem!
+    @IBOutlet var settingsButton: UIBarButtonItem!
+    @IBOutlet var myCardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        myCardView.layer.cornerRadius = 10;
+        myCardView.layer.borderWidth = 1.5;
+        
+        setupBackColorGradient();
     }
-
+    
+    func setupBackColorGradient(){
+        
+        let colorBot = UIColor(red: 200.0 / 255.0, green: 200.0 / 255.0, blue: 192.0 / 255.0, alpha: 1.0).cgColor
+        let colorTop = UIColor(red: 2.0 / 255.0, green: 2.0 / 255.0, blue: 100.0 / 255.0, alpha: 1.0).cgColor
+        
+        let gl = CAGradientLayer()
+        gl.colors = [colorTop, colorBot]
+        gl.locations = [0.0, 1.0]
+        
+        self.view.backgroundColor = UIColor.clear;
+        let backgroundLayer = gl
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, at: 0)
+    }
 
 }
 
